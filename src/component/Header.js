6c0c17b6f192f046179b93978741f50e80a1f3ef
Alex <contact.alexandremace@gmail.com>
2021-12-21 15:52:04 +0100

    Update header link

--- src/component/Header.js
+++ src/component/Header.js
@@ -46,7 +46,7 @@ const Header = ({mode, setMode}) => {
                 </a>
                 <div className={"climate-lab d-flex align-items-center"}>
                     <div className={"mr-3"}>un outil du </div>
-                    <a className={"d-flex align-items-center climate-lab-link"} href="https://climatelab.fr"><img height={27} width={27} className={"logo-img mr-1"} src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/alembic_2697-fe0f.png" alt="Alambic"/> ClimateLab</a>
+                    <a target="_blank" rel="noopener noreferrer" className={"d-flex align-items-center climate-lab-link"} href="https://climatelab.fr"><img height={27} width={27} className={"logo-img mr-1"} src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/alembic_2697-fe0f.png" alt="Alambic"/> ClimateLab</a>
                 </div>
                     {/*<div className={"mobile-header"}>*/}
                     {/*    <Button onClick={toggleDrawer()}>Menu</Button>*/}
